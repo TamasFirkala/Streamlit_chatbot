6ed4264bec8f62a4d35bf02c920ea75acf2d72bf
TamasFirkala <77664254+TamasFirkala@users.noreply.github.com>
2023-10-27 13:26:37 +0200

    Update Streamlit_chatbot.py

--- Streamlit_chatbot.py
+++ Streamlit_chatbot.py
@@ -6,7 +6,7 @@ import openai
 
 #import openai
 
-os.environ['API_KEY'] = st.secrets['API_KEY']
+#os.environ['API_KEY'] = st.secrets['API_KEY']
 
 #openai.api_key  = 'API_KEY'
 
@@ -26,7 +26,7 @@ if st.button("Submit"):
     else:
         try:
 
-            #openai.api_key = os.environ["API_KEY"]
+            openai.api_key = st.secrets['API_KEY']
 
             documents = SimpleDirectoryReader('./data').load_data()        
 
