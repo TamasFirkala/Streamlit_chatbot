2b4fe6cb9a9347ab8d2919c3bd2a470e5b4d3917
TamasFirkala <77664254+TamasFirkala@users.noreply.github.com>
2023-10-28 04:26:02 +0200

    Update Streamlit_chatbot.py

--- Streamlit_chatbot.py
+++ Streamlit_chatbot.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 openai.api_key = st.secrets["api_secret"]
 
-st.title("Ask Llama")
+st.title("Hi I am Bela, a Hungarian chatbot. I am presently being trained to help you with chemistry. Don't hesitate to ask me!")
 
 query = st.text_input("What would you like to ask? (source: ./data)", "")
 
