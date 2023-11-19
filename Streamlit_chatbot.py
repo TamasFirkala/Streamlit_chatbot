f817fe9af849313f8b3186a5dd3e2b994786f132
TamasFirkala <77664254+TamasFirkala@users.noreply.github.com>
2023-11-19 23:08:25 +0100

    Update Streamlit_chatbot.py

--- Streamlit_chatbot.py
+++ Streamlit_chatbot.py
@@ -19,12 +19,27 @@ if st.button("Submit"):
 
             documents = SimpleDirectoryReader('./data').load_data()
 
-            from llama_index import GPTVectorStoreIndex
+            llm_predictor = LLMPredictor(llm=OpenAI(temperature=0.1, model_name="gpt-4-1106-preview"))
+
+            max_input_size = 4096
+            num_output = 256
+            max_chunk_overlap = 0.5
+            prompt_helper = PromptHelper(max_input_size, num_output, max_chunk_overlap)
+
+            custom_LLM_index = GPTVectorStoreIndex(documents, llm_predictor=llm_predictor, prompt_helper=prompt_helper)
+
+            query_engine = custom_LLM_index.as_query_engine()
+
+            response = query_engine.query(query)
+
+            
+
+            """from llama_index import GPTVectorStoreIndex
 
             index = GPTVectorStoreIndex.from_documents(documents)
             query_engine = index.as_query_engine()
 
-            response = query_engine.query(query)
+            response = query_engine.query(query)"""
 
 
 
